0fa7258e852d59d2648edaba8b30c67d9dceb451
Mario Weiler <mariow@arizona.edu>
2023-09-18 13:23:46 -0700

    Updated Pharm

--- internal/models/pharmacy.go
+++ internal/models/pharmacy.go
@@ -1,32 +1,46 @@
 package models
 
-import "fmt"
-
 type Pharmacy struct {
-    name     string
-    location string
-    isOpen   bool
+	name         string
+	location     string
+	isOpen       bool
+	website      string
+	owner        string
+	phoneNumber  int
+	workingHours string
+	isClosed     bool
 }
 
 // Constructor function for creating a new Pharmacy instance
-func NewPharmacy(name, location string, isOpen bool, website string, owner string, phone-number int, working-hours string) *Pharmacy {
-    return &Pharmacy{
-        name:     name,
-        location: location,
-        website: website,
-        owner: owner,
-        phone-number: phone-number,
-        working-hours: working hours,
-        isOpen:   isOpen,
-    }
+func NewPharmacy(name, location string, isOpen bool, website string, owner string, phoneNumber int, workingHours string, isClosed bool) *Pharmacy {
+	return &Pharmacy{
+		name:         name,
+		location:     location,
+		website:      website,
+		owner:        owner,
+		phoneNumber:  phoneNumber,
+		workingHours: workingHours,
+		isOpen:       isOpen,
+		isClosed:     isClosed,
+	}
 }
 
 // Setter method to update the isOpen status of the Pharmacy
 func (p *Pharmacy) SetIsOpen(isOpen bool) {
-    p.isOpen = isOpen
+	p.isOpen = isOpen
 }
 
 // Getter method to retrieve the isOpen status of the Pharmacy
 func (p *Pharmacy) GetIsOpen() bool {
-    return p.isOpen
+	return p.isOpen
+}
+
+// Setter method to update the isOpen status of the Pharmacy
+func (p *Pharmacy) SetIsClosed(isClosed bool) {
+	p.isClosed = isClosed
+}
+
+// Getter method to retrieve the isOpen status of the Pharmacy
+func (p *Pharmacy) GetIsClosed() bool {
+	return p.isClosed
 }
