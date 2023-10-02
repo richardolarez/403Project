45ddb4d5dfe244de26b5f25b31e75e9396df3bc2
Javi <70613839+cjavierr@users.noreply.github.com>
2023-10-02 13:34:06 -0700

    Template GUI from online guide, can be used as an example or a base. Has basic FE features and has persistent storage through a local JSON server

---
+++ web/app/src/components/layouts/sidebar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Menu } from 'antd';
+import {
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+  } from '@ant-design/icons';
+import {useNavigate}  from 'react-router';
+const SideNav = () => {
+    const history = useNavigate();
+const handleUserClick = () => {
+        history('/list');
+    }
+const handleVideosClick = () => {
+        history('/videos');
+    }
+const handleFileClick = () => {
+        history('/files');
+    }
+return (
+      <div>
+        <div style={{height: "32px", background: "rgba(255, 255, 255, 0.2)", margin: "16px"}}></div>
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                <Menu.Item key="1" onClick={handleUserClick}>
+                    <UserOutlined />
+                    <span> Users</span>
+                </Menu.Item>
+                <Menu.Item key="2" onClick={handleVideosClick}>
+                    <VideoCameraOutlined />
+                    <span> Videos</span>
+                </Menu.Item>
+                <Menu.Item key="3" onClick={handleFileClick}>
+                    <UploadOutlined />
+                    <span> Files</span>
+                </Menu.Item>
+            </Menu>
+        </div>
+  );
+}
+export default SideNav;
---
+++ web/app/src/config/ApplicationRoutes.tsx
@@ -0,0 +1,53 @@
+//src/config/ApplicationRoutes.tsx
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import List from "../components/pages/list";
+import Form from "../components/pages/form";
+import SideNav from "../components/layouts/sidebar";
+import File from "../components/pages/files";
+import Videos from "../components/pages/videos";
+import { Layout } from 'antd';
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined
+  } from '@ant-design/icons';
+
+const { Header, Sider, Content} = Layout;
+const ApplicationRoutes = () => {
+  const [collapse, setCollapse] = useState(false);
+useEffect(() => {
+    window.innerWidth <= 760 ? setCollapse(true) : setCollapse(false);
+  }, []);
+const handleToggle = (event: any) => {
+        event.preventDefault();
+        collapse ? setCollapse(false) : setCollapse(true);
+    }
+  return (
+      <Router>
+        <Layout>
+          <Sider trigger={null} collapsible collapsed={collapse}>
+            <SideNav />
+          </Sider>
+          <Layout>
+            <Header className="siteLayoutBackground" style={{padding: 0, background: "#001529"}}>
+                      {React.createElement(collapse ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                          className: 'trigger',
+                          onClick: handleToggle,
+                          style: {color: "#fff"}
+                      })}
+            </Header>
+              <Content style={{margin: '24px 16px', padding: 24, minHeight: "calc(100vh - 114px)", background: "#fff"}}>
+                <Routes>
+                    <Route path="/list" Component={List} />
+                    <Route path="/form" Component={Form} />
+                    <Route path="/files" Component={File} />
+                    <Route path="/videos" Component={Videos} />
+                    
+                </Routes>
+              </Content>
+          </Layout>
+        </Layout>
+    </Router>
+  );
+}
+export default ApplicationRoutes;
