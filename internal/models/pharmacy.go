c8bc23bdf624c9efa2e4142590c9fd4c76a488c6
Richardo Larez <richardolarez@arizona.edu>
2023-10-02 19:17:08 -0700

    Corrected pharmacy.go and added unit tests

--- internal/models/pharmacy.go
+++ internal/models/pharmacy.go
@@ -23,6 +23,58 @@ func NewPharmacy(name, location string, isOpen bool, website string, owner strin
 	}
 }
 
+// Getter methods for all attributes
+
+func (p *Pharmacy) GetName() string {
+	return p.name
+}
+
+func (p *Pharmacy) GetLocation() string {
+	return p.location
+}
+
+func (p *Pharmacy) GetWebsite() string {
+	return p.website
+}
+
+func (p *Pharmacy) GetOwner() string {
+	return p.owner
+}
+
+func (p *Pharmacy) GetPhoneNumber() int {
+	return p.phoneNumber
+}
+
+func (p *Pharmacy) GetWorkingHours() string {
+	return p.workingHours
+}
+
+// Setter methods for all attributes
+
+func (p *Pharmacy) SetName(name string) {
+	p.name = name
+}
+
+func (p *Pharmacy) SetLocation(location string) {
+	p.location = location
+}
+
+func (p *Pharmacy) SetWebsite(website string) {
+	p.website = website
+}
+
+func (p *Pharmacy) SetOwner(owner string) {
+	p.owner = owner
+}
+
+func (p *Pharmacy) SetPhoneNumber(phoneNumber int) {
+	p.phoneNumber = phoneNumber
+}
+
+func (p *Pharmacy) SetWorkingHours(workingHours string) {
+	p.workingHours = workingHours
+}
+
 // Setter method to update the isOpen status of the Pharmacy
 func (p *Pharmacy) SetIsOpen(isOpen bool) {
 	p.isOpen = isOpen
---
+++ internal/models/pharmacy_test.go
@@ -0,0 +1,81 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestPharmacy(t *testing.T) {
+	// Create a Pharmacy instance for testing
+	pharmacy := NewPharmacy("Test Pharmacy", "Test Location", true, "test.com", "Test Owner", 1234567890, "9:00 AM - 5:00 PM")
+
+	// Test getters and setters for all attributes
+	t.Run("Test getters and setters", func(t *testing.T) {
+		// Test getters
+		if pharmacy.GetName() != "Test Pharmacy" {
+			t.Error("Expected name to be 'Test Pharmacy'")
+		}
+
+		if pharmacy.GetLocation() != "Test Location" {
+			t.Error("Expected location to be 'Test Location'")
+		}
+
+		if pharmacy.GetWebsite() != "test.com" {
+			t.Error("Expected website to be 'test.com'")
+		}
+
+		if pharmacy.GetOwner() != "Test Owner" {
+			t.Error("Expected owner to be 'Test Owner'")
+		}
+
+		if pharmacy.GetPhoneNumber() != 1234567890 {
+			t.Error("Expected phoneNumber to be 1234567890")
+		}
+
+		if pharmacy.GetWorkingHours() != "9:00 AM - 5:00 PM" {
+			t.Error("Expected workingHours to be '9:00 AM - 5:00 PM'")
+		}
+
+		// Test setters
+		pharmacy.SetName("Updated Pharmacy Name")
+		if pharmacy.GetName() != "Updated Pharmacy Name" {
+			t.Error("Expected updated name to be 'Updated Pharmacy Name'")
+		}
+
+		pharmacy.SetLocation("Updated Location")
+		if pharmacy.GetLocation() != "Updated Location" {
+			t.Error("Expected updated location to be 'Updated Location'")
+		}
+
+		pharmacy.SetWebsite("updated.com")
+		if pharmacy.GetWebsite() != "updated.com" {
+			t.Error("Expected updated website to be 'updated.com'")
+		}
+
+		pharmacy.SetOwner("Updated Owner")
+		if pharmacy.GetOwner() != "Updated Owner" {
+			t.Error("Expected updated owner to be 'Updated Owner'")
+		}
+
+		pharmacy.SetPhoneNumber(987654321)
+		if pharmacy.GetPhoneNumber() != 987654321 {
+			t.Error("Expected updated phoneNumber to be 987654321")
+		}
+
+		pharmacy.SetWorkingHours("10:00 AM - 6:00 PM")
+		if pharmacy.GetWorkingHours() != "10:00 AM - 6:00 PM" {
+			t.Error("Expected updated workingHours to be '10:00 AM - 6:00 PM'")
+		}
+	})
+
+	// Test isOpen getter and setter
+	t.Run("Test isOpen getter and setter", func(t *testing.T) {
+		if pharmacy.GetIsOpen() != true {
+			t.Error("Expected isOpen to be true")
+		}
+
+		pharmacy.SetIsOpen(false)
+		if pharmacy.GetIsOpen() != false {
+			t.Error("Expected updated isOpen to be false")
+		}
+	})
+}
