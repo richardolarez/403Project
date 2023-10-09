28a7e18134adf077581b79fb4e64e2a39815d554
Richardo Larez <richardolarez@arizona.edu>
2023-10-09 00:17:57 -0700

    Created inventory object and db initializer is modified

--- init/db_init.go
+++ init/db_init.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 
-	// Import the Pharmacy struct from pharmacy.go in the same directory
 	"github.com/SFWE403/UArizonaPharmacy/internal/models"
 )
 
@@ -15,27 +14,55 @@ func InitializeDatabase() error {
 	pharmacy1 := models.NewPharmacy("Pharmacy A", "Location A", true, "pharmacyA.com", "Owner A", 1234567890, "9:00 AM - 5:00 PM")
 	pharmacy2 := models.NewPharmacy("Pharmacy B", "Location B", false, "pharmacyB.com", "Owner B", 9876543210, "10:00 AM - 6:00 PM")
 
+	// Create sample InventoryItems
+	item1 := models.NewInventoryItem(1, "Item 1", "Description of Item 1", 10.99, 100)
+	item2 := models.NewInventoryItem(2, "Item 2", "Description of Item 2", 5.99, 50)
+
+	// Create sample Medicines
+	medicine1 := models.NewInventoryItem(3, "Medicine 1", "Description of Medicine 1", 15.99, 200)
+	medicine2 := models.NewInventoryItem(4, "Medicine 2", "Description of Medicine 2", 8.99, 75)
+
 	// Create an array of Pharmacy instances
 	pharmacies := []*models.Pharmacy{pharmacy1, pharmacy2}
 
+	// Create an array of InventoryItems
+	items := []*models.InventoryItem{item1, item2, medicine1, medicine2}
+
 	// Serialize the pharmacies array to JSON
-	jsonData, err := json.MarshalIndent(pharmacies, "", "  ")
+	pharmaciesData, err := json.MarshalIndent(pharmacies, "", "  ")
 	if err != nil {
 		return err
 	}
 
-	// Write JSON data to a file
-	file, err := os.Create("./db/pharmacies.json")
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = file.Write(jsonData)
+	// Serialize the items array to JSON
+	itemsData, err := json.MarshalIndent(items, "", "  ")
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("JSON database initialized and saved to 'pharmacies.json'")
+	// Write JSON data to respective files
+	pharmaciesFile, err := os.Create("./db/pharmacies.json")
+	if err != nil {
+		return err
+	}
+	defer pharmaciesFile.Close()
+
+	_, err = pharmaciesFile.Write(pharmaciesData)
+	if err != nil {
+		return err
+	}
+
+	itemsFile, err := os.Create("./db/items.json")
+	if err != nil {
+		return err
+	}
+	defer itemsFile.Close()
+
+	_, err = itemsFile.Write(itemsData)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("JSON database initialized and saved to 'pharmacies.json' and 'items.json'")
 	return nil
 }
