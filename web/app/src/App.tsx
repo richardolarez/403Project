f13de38d78ceccb3137e7a261068a57bdb2226b3
Richardo Larez <richardolarez@arizona.edu>
2023-10-11 20:01:20 -0700

    Started auth

--- web/app/src/App.tsx
+++ web/app/src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './Login.css';
 
-function App() {
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    // Call the /login endpoint to authenticate the employee
+    const response = await fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    });
+
+    if (response.ok) {
+      const employee = await response.json();
+      console.log('Authenticated employee:', employee);
+      // TODO: Handle authenticated employee
+    } else {
+      console.error('Failed to authenticate employee:', response.statusText);
+      // TODO: Handle authentication error
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={event => setUsername(event.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
+        </label>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default Login;
