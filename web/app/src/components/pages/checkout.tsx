8bccffc32b2cdebefa996929d3750d0b43cbcacb
Antony Mangala <antony.mangala@gmail.com>
2023-10-15 20:21:16 -0700

    Frontend adding items to a cart and displaying them. Still needs backend functionality

--- web/app/src/components/pages/checkout.tsx
+++ web/app/src/components/pages/checkout.tsx
@@ -1,35 +1,52 @@
-import React from 'react';
-import { Form, Button, Input } from 'antd';
+import React, { useState } from 'react';
+import { Form, Button, Input, List } from 'antd';
 import { UserOutlined, FieldNumberOutlined,  NumberOutlined } from '@ant-design/icons';
+
+interface cartItem{
+customerid: number;
+itemid: number;
+quantity: number;
+}
+
 
 
 const Checkout = () => {
 
+  const [cartItems, setCartItems] = useState<cartItem[]>([]);
+  const [form] = Form.useForm();
+
+  const addItemToCart = (values: { customerid: number, itemid: number, quantity: number }) => {
+    setCartItems([...cartItems, {
+        customerid: values.customerid,
+        itemid: values.itemid,
+        quantity: values.quantity
+    }]);
+};
+
   const onFinish = (values: { customerid: number, itemid: number, quantity: number }) => {
-    // Call the /login endpoint to authenticate the employee
-    // fetch('http://localhost:8080/checkout', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({ username: values.username, password: values.password })
-    // })
-    // .then(response => {
-    //   if (!response.ok) {
-    //     throw new Error(`HTTP error! status: ${response.status}`);
-    //   }
-    //   return response.json();
-    // })
-    // .then(employee => {
-    //   console.log('Authenticated employee:', employee);
-    //   sessionStorage.setItem('authenticated', 'true');
-    //   sessionStorage.setItem('UserFName', employee.FirstName);
-    //   window.location.reload();
-    // })
-    // .catch(error => {
-    //   console.error('Failed to u', error);
-    //   // TODO: Handle authentication error
-    // });
+    console.log("onFinish call");
+    fetch('http://localhost:8080/checkout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ customerid: values.customerid, itemid: values.itemid, quantity: values.quantity })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(cartItem => {
+      // Add the item to the cart items state
+      console.log("API response");
+      setCartItems([...cartItems,{customerid: values.customerid, itemid: values.itemid, quantity: values.quantity}]);
+    })
+    .catch(error => {
+      console.error('Failed to u', error);
+      // TODO: Handle authentication error
+    });
   };
 
       return (
@@ -38,6 +55,8 @@ const Checkout = () => {
           name="checkout"
           initialValues={{ remember: true }}
           style={{ width: '40%'}}
+          onFinish={onFinish}
+          form={form}
           >
           <Form.Item
             name="customerid" label="Customer ID"
@@ -54,7 +73,7 @@ const Checkout = () => {
           </Form.Item>
 
           <Form.Item
-            name="qty" label="Qty"
+            name="quantity" label="Qty"
             rules={[{ required: true, message: 'Please provide a quantity' }]}
             style={{ width: '100%'}}
           >
@@ -65,18 +84,28 @@ const Checkout = () => {
           </Form.Item>
 
           <Form.Item>
-            <Button type="default" htmlType="submit" style={{ width: '100%' }}>
+          <Button type="default" onClick={() => addItemToCart(form.getFieldsValue())} style={{ width: '100%' }}>
               Add Item
             </Button>
           </Form.Item>
 
           <Form.Item>
-            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+          <Button type="default" htmlType="submit" style={{ width: '100%' }}>
               Checkout
             </Button>
           </Form.Item>
 
           </Form>
+          <List
+        header={<div>Cart Items</div>}
+        bordered
+        dataSource={cartItems}
+        renderItem={item => (
+          <List.Item>
+            {`Customer ID: ${item.customerid}, Item ID: ${item.itemid}, Quantity: ${item.quantity}`}
+          </List.Item>
+        )}
+      />
         </div>
 
       );
