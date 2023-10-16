1651b584ce99289e4a945f0d4400eb71de4d7912
Javier <70613839+cjavierr@users.noreply.github.com>
2023-10-15 21:29:27 -0700

    Working removal from cart, using UUID library to uniquely identify each cart item

--- web/app/src/components/pages/checkout.tsx
+++ web/app/src/components/pages/checkout.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { Form, Button, Input, List } from 'antd';
 import { UserOutlined, FieldNumberOutlined,  NumberOutlined } from '@ant-design/icons';
+import { v4 as uuid } from 'uuid'
+import '../../App.css'
 
 interface cartItem{
+id: string; // a uuid var, which is a hexadecimal thing
 customerid: number;
 itemid: number;
 quantity: number;
@@ -14,16 +17,33 @@ const Checkout = () => {
 
   const [cartItems, setCartItems] = useState<cartItem[]>([]);
   const [form] = Form.useForm();
+  const [selectedItem, setSelectedItem] = useState<cartItem | null>(null); // Store the selected item
 
-  const addItemToCart = (values: { customerid: number, itemid: number, quantity: number }) => {
+
+  const addItemToCart = (values: { id: string, customerid: number, itemid: number, quantity: number }) => {
+    const itemId = uuid(); // Generate a unique ID for the item
     setCartItems([...cartItems, {
+        id: itemId,
         customerid: values.customerid,
         itemid: values.itemid,
         quantity: values.quantity
     }]);
 };
 
-  const onFinish = (values: { customerid: number, itemid: number, quantity: number }) => {
+  const removeItemFromCart = (cartId: string) => {
+    if (selectedItem) {
+      // Filter out the selected item
+      const updatedCart = cartItems.filter((item) => item !== selectedItem);
+      setCartItems(updatedCart);
+      setSelectedItem(null); // Clear the selected item
+    }
+  };
+
+  const onItemSelected = (item: cartItem) => {
+    setSelectedItem(item); // Set the selected item when it's clicked
+  };
+
+  const onFinish = (values: { id: string, customerid: number, itemid: number, quantity: number }) => {
     console.log("onFinish call");
     fetch('http://localhost:8080/checkout', {
       method: 'POST',
@@ -41,7 +61,7 @@ const Checkout = () => {
     .then(cartItem => {
       // Add the item to the cart items state
       console.log("API response");
-      setCartItems([...cartItems,{customerid: values.customerid, itemid: values.itemid, quantity: values.quantity}]);
+      setCartItems([...cartItems,{id: values.id, customerid: values.customerid, itemid: values.itemid, quantity: values.quantity}]);
     })
     .catch(error => {
       console.error('Failed to u', error);
@@ -90,6 +110,15 @@ const Checkout = () => {
           </Form.Item>
 
           <Form.Item>
+          <Button
+            onClick={() => selectedItem && removeItemFromCart(selectedItem.id)}
+            disabled={!selectedItem}
+          >
+            Remove Selected Item
+          </Button>
+          </Form.Item>
+
+          <Form.Item>
           <Button type="default" htmlType="submit" style={{ width: '100%' }}>
               Checkout
             </Button>
@@ -101,7 +130,9 @@ const Checkout = () => {
         bordered
         dataSource={cartItems}
         renderItem={item => (
-          <List.Item>
+          <List.Item 
+          className={selectedItem && selectedItem.id === item.id ? 'selected-item' : ''}
+          onClick={() => onItemSelected(item)}>
             {`Customer ID: ${item.customerid}, Item ID: ${item.itemid}, Quantity: ${item.quantity}`}
           </List.Item>
         )}
