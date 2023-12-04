9329b1f778ed993b38d4813d1b04dc5605936906
Richardo Larez <richardolarez@arizona.edu>
2023-12-04 05:13:59 -0700

    Adding Antony's Medicine table

--- web/app/src/components/pages/InventoryList.tsx
+++ web/app/src/components/pages/InventoryList.tsx
@@ -15,13 +15,28 @@ interface InventoryItem {
     IsPrescription: boolean;
 }
 
+interface Medicine {
+    ID: number;
+    Drug: string;
+    Doses: number;
+    Price: number;
+    ExpirationDate: Date;
+}
+
 const InventoryList: React.FC = () => {
     const history = useNavigate();
     const [inventoryItems, setInventoryItems] = useState<InventoryItem[]>([]);
+    const [allMedicines, setAllMedicines] = useState<Medicine[]>([]);
     const [selectedItem, setSelectedItem] = useState(false);
+    const [selectedMedicine, setSelectedMedicine] = useState(false);
     const [item, setItem] = useState<InventoryItem | null>(null);
+    const [medicines, setMedicine] = useState<Medicine | null>(null);
     const [updateModalVisible, setUpdateModalVisible] = useState(false);
+    const [reorderModalVisible, setReOrderModalVisible] = useState(false);
+    const [reorderAmount, setReorderAmount] = useState<number>(0);
     const [form] = Form.useForm();
+    const [managerCheck, setLoggedInUserRole] = useState<string | null>(null);
+    
 
     useEffect(() => {
         const fetchInventoryItems = async () => {
@@ -32,6 +47,12 @@ const InventoryList: React.FC = () => {
                 } else {
                     console.error('Failed to fetch inventory items');
                 }
+                const medicineResponse = await axios.get('http://localhost:8080/medicines');
+                if (medicineResponse.status === 200) {
+                    setAllMedicines(medicineResponse.data);
+                } else {
+                    console.error('Failed to fetch medicine data');
+                }
             } catch (error) {
                 console.error('Failed to fetch inventory items:', error);
             }
@@ -39,6 +60,12 @@ const InventoryList: React.FC = () => {
 
         fetchInventoryItems();
     }, []);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8080/employees`).then(res => {
+          setLoggedInUserRole(sessionStorage.getItem("UserRole"));
+        });
+      }, []);
 
     const columns = [
         {
@@ -60,6 +87,33 @@ const InventoryList: React.FC = () => {
        
     ];
 
+    const medicineColumns = [
+        {
+            title: 'ID',
+            dataIndex: 'ID',
+        },
+        {
+            title: 'Drug',
+            dataIndex: 'Drug',
+        },
+        {
+            title: 'Doses',
+            dataIndex: 'Doses',
+        },
+        {
+            title: 'Strength',
+            dataIndex: 'Strength',
+        },
+        {
+            title: 'Price',
+            dataIndex: 'Price',
+        },
+        {
+            title: 'ExpirationDate',
+            dataIndex: 'ExpirationDate',
+        },
+    ];
+
     const handleRowClick = (record: InventoryItem) => {
         setSelectedItem(true);
         setItem(record);
@@ -74,6 +128,27 @@ const InventoryList: React.FC = () => {
 
     const handleAddClick = () => {
         history('/inventory')
+    }
+
+    const handleMedRowClick = (medicineRecord: Medicine) => {
+        setSelectedMedicine(true);
+        setMedicine(medicineRecord);
+        console.log(medicineRecord)
+    };
+
+    const handleMedUpdateClick = () => {
+        setReOrderModalVisible(true);
+        console.log(item)
+    };
+
+    const handleMedReorderClick = () => {
+        setReOrderModalVisible(true);
+        console.log(medicines)
+    };
+
+
+    const handleMedAddClick = () => {
+        
     }
 
     const handleModalOk = (values: any) => {
@@ -91,7 +166,7 @@ const InventoryList: React.FC = () => {
             axios.get('http://localhost:8080/inventory').then((res) => {
               setInventoryItems(res.data);
             });
-            setSelectedItem(false)
+            setSelectedMedicine(false)
           })
           .catch((error) => {
             console.error('Error deleting employee:', error);
@@ -99,8 +174,41 @@ const InventoryList: React.FC = () => {
         setUpdateModalVisible(false);
     };
 
+    const handleMedReOrderModalOk = () => {
+        if (!medicines) {
+            console.error('No  selected item for update');
+            return;
+          } 
+
+          const reorderData = {
+            ID: medicines.ID,
+            Doses: reorderAmount,
+        };
+          console.log(reorderData)
+        axios.post(`http://localhost:8080/updateInventoryItem`, reorderData).then(res => { //Need backend implementation
+            axios.get('http://localhost:8080/medicines').then((res) => {
+              setAllMedicines(res.data);
+            });
+            setSelectedItem(false)
+          })
+          .catch((error) => {
+            console.error('Error deleting employee:', error);
+          });
+          setReOrderModalVisible(false);
+    };
+
     const handleModalCancel = () => {
         setUpdateModalVisible(false);
+    };
+
+    const handleMedReorderCancel = () => {
+        setReOrderModalVisible(false);
+    };
+
+    const handleReorderAmountChange = (value: number | null) => {
+        if (value !== null) {
+            setReorderAmount(value);
+        }
     };
 
     return (
@@ -126,6 +234,35 @@ const InventoryList: React.FC = () => {
                         dataSource={inventoryItems}
                         onRow={(record) => ({
                             onClick: () => handleRowClick(record),
+                        })}
+                    />
+                </Col>
+            </Row>
+            <Row gutter={[40, 0]}>
+                <Col span={24}>
+                <Title level={3}>Medications</Title>
+                <Col span={18}>
+                    {selectedMedicine && <Title level={3}>Selected Medicine: {medicines?.Drug}</Title>}
+                </Col>
+                <Col span={2}>
+                    <Button block onClick={handleMedAddClick}>Add</Button>
+                </Col>
+                <Col span={2}>
+                {managerCheck === "Manager" && (
+                    <Button block onClick={handleMedUpdateClick}>Update</Button>
+                )}
+                </Col>
+                <Col span={2}>
+                {managerCheck === "Manager" && (
+                    <Button block onClick={handleMedUpdateClick}>Re-Order</Button>
+                )}
+                </Col>
+                
+                    <Table
+                        columns={medicineColumns}
+                        dataSource={allMedicines}
+                        onRow={(medicineRecord) => ({
+                            onClick: () => handleMedRowClick(medicineRecord),
                         })}
                     />
                 </Col>
@@ -163,6 +300,25 @@ const InventoryList: React.FC = () => {
                     </Form.Item>
                 </Form>
             </Modal>
+            <Modal
+                title="Reorder Medicine"
+                visible={reorderModalVisible}
+                onCancel={handleMedReorderCancel}
+                onOk={handleMedReOrderModalOk}
+            >
+                <Form layout="vertical">
+                    <Form.Item label="Reorder Amount" name="ReorderAmount">
+                        <InputNumber
+                            min={1}
+                            value={reorderAmount}
+                            onChange={handleReorderAmountChange}
+                            style={{ width: '100%' }}
+                        />
+                    </Form.Item>
+                </Form>
+            </Modal>
+
+            
 
         </div>
     );
